c6ac9684840bae8ce060a10202d9e8d987ce4fea
Abrar Hayat <abrarhayat@gmail.com>
2024-07-09 15:09:35 +1000

    Added a new function to fetch file content from a given pull request

--- bitbucket_api.py
+++ bitbucket_api.py
@@ -26,3 +26,31 @@ def get_all_file_content(bitbucket: Bitbucket, project_key: str,
         all_repo_file_content[filename] = file_content
     return all_repo_file_content
 
+def get_files_from_pull_request(bitbucket: Bitbucket, project_key: str,
+                                repository_slug: str, pull_request_id: int,
+                                file_types: list = None) -> dict:
+    """
+    Fetches the content of all files in a given pull request.
+
+    Args:
+        bitbucket (Bitbucket): An instance of the Bitbucket class.
+        project_key (str): The key of the project in Bitbucket.
+        repository_slug (str): The slug (URL-friendly version of the name) 
+        of the repository in Bitbucket.
+        pull_request_id (int): The ID of the pull request in Bitbucket.
+        file_types (list): A list of file types to filter the files by.
+
+    Returns:
+        dict: A dictionary where the keys are the filenames 
+        and the values are the respective file contents.
+    """
+    pull_request = bitbucket.get_pull_request(project_key, repository_slug, pull_request_id)
+    latest_commit_id = pull_request['fromRef']['latestCommit']
+    pr_changes = bitbucket.get_pull_requests_changes(project_key, repository_slug, pull_request_id)
+    pr_files = defaultdict()
+    for change in pr_changes:
+        file_name = change['path']['toString']
+        if(file_types and file_name.split('.')[-1] not in file_types):
+            continue
+        pr_files[file_name] = bitbucket.get_content_of_file(project_key, repository_slug, file_name, latest_commit_id) 
+    return pr_files
